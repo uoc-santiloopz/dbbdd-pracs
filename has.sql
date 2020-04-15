18b12993fe4d566b50e739622f1992a2d782e408
thir13en <santilpzate@gmail.com>
2020-04-15 05:22:31 +0200

    added client and has

---
+++ has.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Has (
+	NIF CHAR(10),
+	accountNumber INTEGER,
+	CONSTRAINT PK_Has PRIMARY KEY (NIF, accountNumber),
+	CONSTRAINT FK_ClientHas FOREIGN KEY (NIF) REFERENCES Client(NIF),
+	CONSTRAINT FK_AccountHas FOREIGN KEY (loanCode) REFERENCES Account(number)
+);
