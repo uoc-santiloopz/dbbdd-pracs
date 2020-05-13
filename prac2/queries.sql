e145b962fd512714be42331865768f3ce9df0df4
thir13en <santilpzate@gmail.com>
2020-05-13 03:18:02 +0200

    added some queries as the exercise progresses

--- prac2/queries.sql
+++ prac2/queries.sql
@@ -1,5 +1,18 @@
-# 2-1
-SELECT c.name AS company_name, s.name AS ship_name, r.codeinstallation AS code_restaurant
-FROM company c, ship s, restaurant r
-WHERE c.id=s.idcompany
-AND s.idcompany=r.codeinstallation;
+SELECT c.name AS company_name, s.name AS ship_name, r.codeinstallation AS code_restaulrant
+FROM company c
+INNER JOIN ship s ON s.idcompany = c.id
+INNER JOIN restaurant r ON r.nameship = s.name
+INNER JOIN installation i ON i.code = r.codeinstallation
+INNER JOIN isspecializedin iss ON iss.coderestaurant = r.codeinstallation
+INNER JOIN style st ON iss.codestyle = st.code AND st.name = 'Mediterranean cuisine'
+ORDER BY c.name, s.name;
+
+CREATE VIEW V_Mediterranean(company_name, ship_name, code_restaurant) AS
+    SELECT c.name, s.name, r.codeinstallation
+    FROM company c
+    INNER JOIN ship s ON s.idcompany = c.id
+    INNER JOIN restaurant r ON r.nameship = s.name
+    INNER JOIN installation i ON i.code = r.codeinstallation
+    INNER JOIN isspecializedin iss ON iss.coderestaurant = r.codeinstallation
+    INNER JOIN style st ON iss.codestyle = st.code AND st.name = 'Mediterranean cuisine'
+    ORDER BY c.name, s.name;
